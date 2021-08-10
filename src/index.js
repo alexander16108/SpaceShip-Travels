761003a2040e2a16cc705dde7949370c1d315fd0
sneeu-leeu <sa070197@gmail.com>
2021-08-10 14:09:14 +0200

    index.html setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import _ from 'lodash';
+
+const newElem = (elem) => document.createElement(elem);
+const newDiv = () => document.createElement('div');
+const grab = (elem) => document.getElementById(elem);
+
+const populate = () => {
+ 
+}
