b081cca951f4d4c131eb6ce26f83486b6ca3b3a5
sneeu-leeu <sa070197@gmail.com>
2021-08-10 15:14:45 +0200

    Create header

---
+++ src/api.js
@@ -0,0 +1,1 @@
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import _ from 'lodash';
+import './style.css';
+import Logo from './pokeLogo.png';
 
 const newElem = (elem) => document.createElement(elem);
 const newDiv = () => document.createElement('div');
 const grab = (elem) => document.getElementById(elem);
 
 const populate = () => {
- 
-}
+  // logo
+  const logoDiv = grab('logo-div');
+  const logo = new Image();
+  logo.id = 'logoImg';
+  logo.src = Logo;
+
+  logoDiv.append(logo);
+
+  // nav-list
+  const navLi = grab('nav-list');
+  navLi.classList.add('list-style');
+
+  const li1 = newElem('li');
+  li1.innerText = 'Pokemon';
+  li1.classList.add('nav-list-txt');
+
+  const li2 = newElem('li');
+  li2.innerText = 'Comments';
+  li2.classList.add('nav-list-txt');
+
+  navLi.append(li1, li2);
+
+};
+
+document.addEventListener('DOMContentLoaded', populate);
