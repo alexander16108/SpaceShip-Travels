820e9755947ae6a8e554ff51361a9d9cfecf0787
sneeu-leeu <sa070197@gmail.com>
2021-08-10 17:41:31 +0200

    Create function to get pokemon

--- src/api.js
+++ src/api.js
@@ -1,1 +1,10 @@
-const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+const baseUrl = 'https://pokeapi.co/api/v2/';
+
+const getPokemon = async (pokeName) => {
+  const url = `${baseUrl}/pokemon/${pokeName}`;
+  let pokemon = await fetch(url);
+  pokemon = await pokemon.json();
+  return pokemon;
+};
+
+export default { getPokemon };
