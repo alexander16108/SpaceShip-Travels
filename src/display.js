0c65f2bb75592b64b1aed376c93317b0f6534cc7
alexander16108 <alexandermayowa05@gmail.com>
2021-08-12 06:09:17 +0100

    correcting mistake in display.js file

--- src/display.js
+++ src/display.js
@@ -1,6 +1,62 @@
-const commentLoader = (data) => {
-  const para = document.createElement('p');
-  para.innerHTML = `${data.creation_date} ${data.username}: ${data.comment}`;
-  return para;
+import { newElem, newDiv, grab } from './support.js';
+
+class PokemonContainer {
+  constructor(pokemonImage, pokemonName, pokemonLikes, pokemonId) {
+    this.pokemonImage = pokemonImage;
+    this.pokemonName = pokemonName;
+    this.pokemonLikes = `${pokemonLikes} Likes`;
+    this.pokemonId = pokemonId;
+  }
+
+  display() {
+    const pokeDisplay = grab('Pokemon-display');
+    pokeDisplay.classList.add('grid-container');
+
+    const container = newDiv();
+    container.classList.add('grid-item');
+
+    const pokemonImg = newElem('img');
+    pokemonImg.src = this.pokemonImage;
+    pokemonImg.classList.add('pokemon-img');
+
+    const pokemonName = newElem('p');
+    pokemonName.classList.add('m-bot-5', 'name-txt');
+    pokemonName.innerText = this.pokemonName.toUpperCase();
+    const numLikes = newElem('p');
+    numLikes.classList.add('m-bot-5');
+    numLikes.innerText = this.pokemonLikes;
+
+    const likeButton = newElem('i');
+    likeButton.classList.add('far', 'fa-heart');
+
+    const infoDiv = newDiv();
+    infoDiv.classList.add('flex', 'info-div-align');
+    infoDiv.append(likeButton, numLikes);
+
+    const commentButton = newElem('button');
+    commentButton.classList.add('m-top-5', 'm-bot-5', 'coment-btn');
+    commentButton.innerText = 'Comment';
+
+    // commentButton.addEventListener('click', () => {
+    //   display();
+    //   onPressCommentsButton(id);
+    // })
+
+    container.append(pokemonImg, pokemonName, infoDiv, commentButton);
+    pokeDisplay.append(container);
+  }
+}
+
+const displayPokemon = (pokemon) => {
+  pokemon.forEach((pokes) => {
+    const pokemonContainer = new PokemonContainer(
+      pokes.sprites.other.dream_world.front_default,
+      pokes.name,
+      0,
+      pokes.id,
+    );
+    pokemonContainer.display();
+  });
 };
-export default commentLoader;
+
+export default displayPokemon;
