bb6b963f163710cf1bfc618dfa910d8b5c00cdff
sneeu-leeu <sa070197@gmail.com>
2021-08-12 13:17:09 +0200

    Add likes to PokemonContainer class and display

--- src/display.js
+++ src/display.js
@@ -1,3 +1,4 @@
+import { pokemonLove, fetchLove } from './likeAPI.js';
 import { newElem, newDiv, grab } from './support.js';
 
 class PokemonContainer {
@@ -9,7 +10,6 @@ class PokemonContainer {
     this.updateLikes.numLikesDisplay = null;
     this.pokemonId = pokemonId;
   }
-
 
   updateLikes() {
     this.numLikesDisplay.innerText = this.showLikes === 1 ? `${this.showLikes} Like` : `${this.showLikes} Likes`;
@@ -23,42 +23,57 @@ class PokemonContainer {
     container.classList.add('grid-item');
 
     const pokemonImg = newElem('img');
-    pokemonImg.src = this.pokemonImage;
     pokemonImg.classList.add('pokemon-img');
+    pokemonImg.src = this.pokemonImage;
 
     const pokemonName = newElem('p');
     pokemonName.classList.add('m-bot-5', 'name-txt');
     pokemonName.innerText = this.pokemonName.toUpperCase();
-    const numLikes = newElem('p');
-    numLikes.classList.add('m-bot-5');
-    numLikes.innerText = this.pokemonLikes;
+    const likes = newElem('p');
+    likes.classList.add('m-bot-5');
+    likes.innerText = this.pokemonLikes;
 
-    const likeButton = newElem('i');
-    likeButton.classList.add('far', 'fa-heart');
+    this.numLikesDisplay = likes;
+
+    const likeBtn = newElem('i');
+    likeBtn.classList.add('far', 'fa-heart');
+    likeBtn.addEventListener('click', async () => {
+      const status = await pokemonLove(this.pokemonId);
+      if (status === 201) {
+        this.showLikes += 1;
+        this.updateLikes();
+      }
+    });
 
     const infoDiv = newDiv();
     infoDiv.classList.add('flex', 'info-div-align');
-    infoDiv.append(likeButton, numLikes);
+    infoDiv.append(likeBtn, likes);
 
     const commentButton = newElem('button');
     commentButton.classList.add('m-top-5', 'm-bot-5', 'coment-btn');
     commentButton.innerText = 'Comment';
 
-    commentButton.addEventListener('click', () => {
-      commentButton.classList.add('popup');
-    });
+    // commentButton.addEventListener('click', () => {
+    //   commentButton.classList.add('popup');
+    // });
 
     container.append(pokemonImg, pokemonName, infoDiv, commentButton);
     pokeDisplay.append(container);
+
+    this.updateLikes();
   }
 }
 
-const displayPokemon = (pokemon) => {
+const displayPokemon = async (pokemon) => {
+  const result = await fetchLove();
   pokemon.forEach((poke) => {
+    let love = 0;
+    love = result.likes.find((item) => item.item_id === pokemon.id)
+      ? result.likes.find((item) => item.item_id === pokemon.id).likes : 0;
     const pokemonContainer = new PokemonContainer(
       poke.sprites.other.dream_world.front_default,
       poke.name,
-      0,
+      love,
       poke.id,
     );
     pokemonContainer.display();
